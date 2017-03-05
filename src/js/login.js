16c9530906008929bd58de16645b0f2f2ba3e4a5
Ani <anihovhannisyang@gmail.com>
2017-03-06 02:38:21 +0400

    Initial commit with server

--- src/js/login.js
+++ src/js/login.js
@@ -72,31 +72,83 @@ var loginUserData = function(data){
   getUserData(data["name"]);
 };
 
+var socket;
+function init() {
+  var host = "ws://127.0.0.1:6767/server/aniws"; // SET THIS TO YOUR SERVER
+  try {
+    socket = new WebSocket(host);
+    log('WebSocket - status '+socket.readyState);
+    socket.onopen    = function(msg) { 
+      log("Welcome - status "+this.readyState); 
+    };
+    socket.onmessage = function(msg) { 
+      log("Received: "+msg.data); 
+    };
+    socket.onclose   = function(msg) { 
+      log("Disconnected - status "+this.readyState); 
+    };
+  } catch(ex){ 
+    log(ex); 
+  }
+  $("msg").focus();
+};
+function send(){
+  var txt,msg;
+  txt = $("msg");
+  msg = txt.value;
+  if(!msg) { 
+    alert("Message can not be empty"); 
+    return; 
+  }
+  txt.value="";
+  txt.focus();
+  try { 
+    socket.send(msg); 
+    log('Sent: '+msg); 
+  } catch(ex) { 
+    log(ex); 
+  }
+};
+function quit(){
+  if (socket != null) {
+    log("Goodbye!");
+    socket.close();
+    socket=null;
+  }
+};
+function reconnect() {
+  quit();
+  init();
+};
+function log(msg){
+  console.log(msg)
+};
 var getUserData = function(name){
-//	var url = document.location.href + 'api/server.php' + '/test';
-//	$.ajax({
-//		type : "GET",
-//		url : url,
-//		dataType : "json",
-//		success : function(data) {
-//			alert(data);
-//		},
-//		error : function(data) {
-//			console.log("Error in /nodes", data);
-//		}
-//	});
-	//chrome.storage.sync.get(name.toLowerCase(), function(items){
-    //if (chrome.runtime.error) {
-    //  console.log("Fail: Get user name: Runtime error.");
-    //} else {
-      var d = {
-        type: "storage-login",
-        name: name.toLowerCase(),
-        stat: false,
-        items: []
-      };
-      $.event.trigger(d);
-   // }
+	init();
+  //var url = document.location.href + 'api/server.php' + '/test';
+  //  $.ajax({
+  //    type : "GET",
+//    url : url,
+//    dataType : "json",
+//    success : function(data) {
+//      alert(data);
+//    },
+//    error : function(data) {
+//      console.log("Error in /nodes", data);
+//    }
+//  });
+  //chrome.storage.sync.get(name.toLowerCase(), function(items){
+  //if (chrome.runtime.error) {
+  //  console.log("Fail: Get user name: Runtime error.");
+  //} else {
+  //  var d = {
+  //    type: "storage-login",
+  //    name: name.toLowerCase(),
+  //    stat: false,
+  //    items: []
+  //  };
+  //  $.event.trigger(d);
+  // }
   //});
 };
 
